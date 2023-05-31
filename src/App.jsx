1d4115812d43d86ff2914047a44b4cc2aaad3e73
praisex64 <akpevweess1@gmail.com>
2023-05-31 09:52:48 +0100

    feat: navlink component

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React from "react"; 
+import { BrowserRouter, Routes, Route, NavLink, Outlet 
+ } from "react-router-dom";
+
+ function Layout() {
+  const activeStyle = {
+
+     fontWeight: "bold", 
+     textDecoration: "underline", 
+     color: "red"
+
+  }
+
+  return (
+    <div>
+      <h3>Welcome to my page!</h3>
+      <nav>
+        <NavLink 
+         style={({isActive}) => isActive ? activeStyle : null}
+        to="/">Home</NavLink>
+        <NavLink 
+         style={({isActive}) => isActive ? activeStyle : null }
+        to="about">About</NavLink>
+        <NavLink 
+         style={({isActive}) => isActive ? activeStyle : null }
+        to="contact">Contact</NavLink>
+      </nav>
+      <Outlet />
+    </div>
+  )
+ }
+
+ function HomePage() {
+  return (
+    <main>
+      <h2>Home</h2>
+    </main>
+  ); 
+ }
+
+
+ function AboutPage() {
+  return (
+    <main>
+       <h2>About Me</h2>
+    </main>
+  )
+ }
+
+ function ContactPage() {
+  return (
+    <main>
+      <h2>Contact</h2>
+    </main>
+  )
+ }
+
+
+ function App() {
+   return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+              <Route index element = {<HomePage />} />
+              <Route path="about" element={<AboutPage />} />
+              <Route path="contact" element={<ContactPage />} />
+          </Route>
+      </Routes>
+    </BrowserRouter>
+   )
+
+ }
+
+ export default App; 
+
